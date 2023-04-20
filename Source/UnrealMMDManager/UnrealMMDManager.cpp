459170d96694e6753dc8dca141835a0b0f6a9798
DearIce <56225620+DearIcer@users.noreply.github.com>
2023-04-21 01:00:49 +0800

    插件代码重构，基于4.27

--- Source/UnrealMMDManager/UnrealMMDManager.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "UnrealMMDManager.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealMMDManager, "UnrealMMDManager" );
